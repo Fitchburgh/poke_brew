fdff919bf76ca16b4a67052ab9f16d4d4822f597
Brandon Fitch <fitchbj@gmail.com>
2016-11-07 17:56:37 -0500

    Config and procfile changes for redis and sidekiq workers

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: bundle exec puma -p $PORT
-worker: bundle exec sidekiq -c 3
+
+worker: bundle exec sidekiq -e production -C config/sidekiq.yml
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,14 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://h:p6aj2b0cu38e4eeuan9b044rt96@ec2-54-221-211-164.compute-1.amazonaws.com:17009" }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: "redis://h:p6aj2b0cu38e4eeuan9b044rt96@ec2-54-221-211-164.compute-1.amazonaws.com:17009" }
+# end
+#
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: "redis://h:p6aj2b0cu38e4eeuan9b044rt96@ec2-54-221-211-164.compute-1.amazonaws.com:17009" }
+# end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://h:p6aj2b0cu38e4eeuan9b044rt96@ec2-54-221-211-164.compute-1.amazonaws.com:17009" }
-end
+development:
+  :concurrency: 5
+production:
+  :concurrency: 20
+:queues:
+  - default
