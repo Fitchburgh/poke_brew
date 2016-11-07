75f21eea231956c7088b9d3f097e38d20ede743e
Brandon Fitch <fitchbj@gmail.com>
2016-11-07 16:04:54 -0500

    Added redis sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: 'redis://localhost:6379/0' }
-# end
-#
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: 'redis://localhost:6379/0' }
-# end
+Sidekiq.configure_server do |config|
+  config.redis = { url: "redis://h:p6aj2b0cu38e4eeuan9b044rt96@ec2-54-221-211-164.compute-1.amazonaws.com:17009" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://h:p6aj2b0cu38e4eeuan9b044rt96@ec2-54-221-211-164.compute-1.amazonaws.com:17009" }
+end
