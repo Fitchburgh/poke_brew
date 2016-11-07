4be1a2704fad04621f5ea722abb12053cf5aada3
Brandon Fitch <fitchbj@gmail.com>
2016-11-07 15:41:47 -0500

    Changed root route and a controller option

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,11 +1,11 @@
  #
 class GameController < ApplicationController
   def index
+    redirect_to '/trainers/sign_in' if current_trainer.nil?
   end
 
   def home
-    # this feels primitive.  Plan is to have it root to the sign in, and have a navbar to sign in, create new, and have redirects based on button click in JS?
-    redirect_to '/trainers/sign_in' if current_trainer.nil?
+
   end
 
   def loadout
