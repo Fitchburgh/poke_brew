2fb85b99f5a3e532c812d7aa1084742703f501f0
Brandon Fitch <fitchbj@gmail.com>
2016-11-09 16:09:52 -0500

    partitioned more worker space for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,16 @@
 redis_url = ENV['REDIS_URL']
 
-  Sidekiq.configure_server do |config|
-    config.redis = {
-      url: redis_url,
-      namespace: 'workers',
-      size: 2
-    }
-  end
-  Sidekiq.configure_client do |config|
-    config.redis = {
-      url: redis_url,
-      namespace: 'workers',
-      size: 1
-    }
-  end
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: redis_url,
+    namespace: 'workers',
+    size: 5
+  }
+end
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: redis_url,
+    namespace: 'workers',
+    size: 5
+  }
+end
