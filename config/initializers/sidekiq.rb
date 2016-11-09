17c2c64f8aa1d0a02cfa5aa956fd77abb2bc18d5
Brandon Fitch <fitchbj@gmail.com>
2016-11-09 16:15:26 -0500

    more workers

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,13 +4,13 @@ Sidekiq.configure_server do |config|
   config.redis = {
     url: redis_url,
     namespace: 'workers',
-    size: 5
+    size: 15
   }
 end
 Sidekiq.configure_client do |config|
   config.redis = {
     url: redis_url,
     namespace: 'workers',
-    size: 5
+    size: 15
   }
 end
