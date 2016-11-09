60066684e00f316cf7dda33d37838b88b13a7b3a
Brandon Fitch <fitchbj@gmail.com>
2016-11-09 16:43:06 -0500

    Removed namespace gem

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,14 +3,12 @@ redis_url = ENV['REDIS_URL']
 Sidekiq.configure_server do |config|
   config.redis = {
     url: redis_url,
-    namespace: 'workers',
     size: 50
   }
 end
 Sidekiq.configure_client do |config|
   config.redis = {
     url: redis_url,
-    namespace: 'workers',
     size: 50
   }
 end
