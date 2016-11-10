3c5c17aa96fd0f47774d839a9822f264cc232f7c
Brandon Fitch <fitchbj@gmail.com>
2016-11-10 14:19:28 -0500

    Revert to: if worker migration does not succeed

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,7 @@ redis_url = ENV['REDIS_URL']
 Sidekiq.configure_server do |config|
   config.redis = {
     url: redis_url,
-    size: 50
+    size: 30
   }
 end
 Sidekiq.configure_client do |config|
